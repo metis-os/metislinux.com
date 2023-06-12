0cd1ec448ec81a2ac71f89128b6c402e34cdd387
metis-os <hey@pwnwriter.xyz>
2023-06-12 14:28:40 +0000

    Update from https://github.com/metis-os/metislinux.org/commit/24c877217f11725c3f57816af1c8bfa96aa95e10

---
+++ assets/boot-with-usb-6d392d69.js
@@ -0,0 +1,1 @@
+import{M as r}from"./index-1cd1c626.js";import{b as e,v as c}from"./entry-client-c290bcb7.js";function o(n){const t=Object.assign({h1:"h1"},r(),n.components);return e(t.h1,{children:"ok"})}function m(n={}){const{wrapper:t}=Object.assign({},r(),n.components);return t?e(t,c(n,{get children(){return e(o,n)}})):o(n)}export{m as default};
