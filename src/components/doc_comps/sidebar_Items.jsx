fdc41cb8f71b223fa27a70064c334a3029b52fd5
codedsprit <roshan0x01@gmail.com>
2023-06-13 10:59:24 +0545

    add: repo // suckless guide

--- src/components/doc_comps/sidebar_Items.jsx
+++ src/components/doc_comps/sidebar_Items.jsx
@@ -32,7 +32,18 @@ const sidebar_Items = [
       ["EWM", "keybindings/ewm"],
     ]
   },
-  ["The Init", "theinit", " i-mdi-cog"],
+
+  {
+    label:["guides","i-mdi-cog"],
+    items:[
+
+      ["The Init", "guides/theinit" ],
+      ["suckless", "guides/patch-suckless" ],
+      ["pwnvim", "guides/pwnvim" ],
+      ["repos", "guides/repos" ],
+
+    ]
+  },
   // ["Features", "features", ""],
   // ["Debug config", "debugging-config", "i-ri-bug-line"],
   ["Contributing", "contribute", "i-mdi-github"],
